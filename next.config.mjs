e87c6ff5ffe73122279d274df9cece485f61d67b
SHIZA TARIQ KHAN <163668326+shiza13@users.noreply.github.com>
2025-08-25 00:01:48 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // or specify exact hostname like 'images.unsplash.com'
+      },
+    ],
+  },
+};
 
 export default nextConfig;
